062262729d26376b05ff195be6e487baecbcdc3b
Younes Bachouche <bachouchemca7@gmail.com>
2022-09-12 04:23:48 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function mouseEmCima(imagem) {
+    imagem.src="img/camisetajersei.png";
+}
+    
+ function mouseFora(imagem) {
+    imagem.src="img/recomendado1.png";
+}
+
+function mouseEmCima2(imagem){
+    imagem.src="img/camisetapolo.png";
+}
+
+function mouseFora2(imagem){
+    imagem.src="img/recomendado2.png";
+}
+
+function mouseEmCima3(imagem){
+    imagem.src="img/bonesport.png";
+}
+
+function mouseFora3(imagem){
+    imagem.src="img/recomendado3.png";
+}
+
+function mouseEmCima4(imagem){
+    imagem.src="img/bonecontrastante.png";
+}
+
+function mouseFora4(imagem){
+    imagem.src="img/recomendado4.png";
+}
